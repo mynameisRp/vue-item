b9956f90eb07dfc8d6a3c85d159393bea1f8b236
byond5924 <1263688292@qq.com>
2019-06-10 11:07:21 +0800

    peng

---
+++ src/components/find/covertitle.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="covertitle">
+        <ul class="nav-list">
+            <router-link to="coverstory" tag="li" style="margin-left:.3rem;" >全部
+                <span class="line"></span>
+            </router-link>
+            <router-link to="basthome" tag="li" @click="changestate()">BEAST HOME</router-link>
+            <router-link to="bastfloral" tag="li">BEAST FLORAL</router-link>
+            <router-link to="littleb" tag="li">LITTLE B</router-link>
+            <router-link to="tbh" tag="li">T-B-H</router-link>
+            <router-link to="naked" tag="li" style="margin-right:.4rem;">NAKED BEAST</router-link>
+        </ul>
+        <router-view>
+           
+        </router-view>
+    </div>
+     
+</template>
+
+<script>
+import Vuex from 'vuex'
+import coverstory from '@components/find/coverstory'
+import basthome from '@components/find/basthome'
+import bastfloral from '@components/find/bastfloral'
+import littleb from '@components/find/littleb'
+import tbh from '@components/find/tbh'
+import naked from '@components/find/naked'
+export default {
+    name:"covertitle",
+    
+    methods: {
+        
+    },
+    components:{
+       basthome,
+       coverstory,
+       bastfloral,
+       littleb,
+       tbh,
+       naked
+    }
+}
+</script>
+
+<style scoped>
+    #covertitle{
+        clear:both;
+        height:.7rem;
+       width:7.5rem;
+        position:fixed;
+        top:.88rem;
+        left:0;
+        z-index:5;
+        background:white;
+        width:7.5rem;
+        overflow-x:scroll;
+       
+       
+    }
+    .nav-list{
+        clear:both;
+        height:.7rem;
+        border-bottom:.01rem solid #e1e1e1;
+        width:max-content;
+        white-space:nowrap;
+          
+         
+    }
+    .nav-list>li{
+        color: #000;
+        font-weight: 700;
+        font-size:.27rem;
+        float:left;
+        margin-left:.8rem;
+        
+        
+    }
+</style>
---
+++ src/views/find/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="find">
+    <Goods/>
+    <Findtop/>
+   
+    <TabBar/>
+    
+  </div>
+</template>
+
+<script>
+import Goods from '@components/find/goods';
+import Findtop from '@components/find/findtop';
+import findcontent from '@components/find/findcontent';
+import TabBar from '@common/tabBar';
+export default {
+    name:'findpage',
+    components:{
+      Goods,
+      Findtop,
+      findcontent,
+      TabBar
+      }
+   
+}
+</script>
+
+<style scoped>
+  #find{
+    font-size:24px;
+  }
+</style>
